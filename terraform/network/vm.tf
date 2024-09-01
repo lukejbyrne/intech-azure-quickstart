f4950bfa8cf9f92c731f9a86690cf4cccfdb1575
David Stewart <lukebyrnee97@gmail.com>
2024-09-01 16:02:56 +0100

    update vms

--- terraform/network/vm.tf
+++ terraform/network/vm.tf
@@ -1,18 +1,29 @@
+resource "azurerm_availability_set" "avset" {
+  name                = "example-aset"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  platform_fault_domain_count  = 2  # Set to 2 or 1
+  platform_update_domain_count = 5  # You can keep this as is
+  managed             = true
+}
+
 # Linux Virtual Machine
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "${random_pet.prefix.id}-vm"
+resource "azurerm_linux_virtual_machine" "vm1" {
+  name                = "${random_pet.prefix.id}-vm1"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  size                = var.vm_size
+  size                = "Standard_B1s"
   admin_username      = "azureuser"
   network_interface_ids = [
-    azurerm_network_interface.nic.id,
+    azurerm_network_interface.nic1.id,
   ]
+
+  availability_set_id = azurerm_availability_set.avset.id
 
   admin_ssh_key {
     username   = "azureuser"
-    public_key = var.public_key_loc
-}
+    public_key = file("${var.public_key_loc}")
+  }
 
   os_disk {
     caching              = "ReadWrite"
@@ -22,7 +33,38 @@ resource "azurerm_linux_virtual_machine" "vm" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
+    sku       = "22_04-LTS"
+    version   = "latest"
+  }
+}
+
+# Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm2" {
+  name                = "${random_pet.prefix.id}-vm2"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  network_interface_ids = [
+    azurerm_network_interface.nic2.id,
+  ]
+
+  availability_set_id = azurerm_availability_set.avset.id
+
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("${var.public_key_loc}")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-LTS"
     version   = "latest"
   }
 }
